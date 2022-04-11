c1442753aaf9f3b1d361c64f868f4e997f8abcb0
Yilikal250 <teshomeyilikal250@gmail.com>
2022-04-11 18:49:46 +0300

    drop dawn menu added

---
+++ script.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line no-unused-vars
+function togglePopup() {
+  // eslint-disable-next-line indent
+  document.getElementById('popup-1').classList.toggle('active');
+}
+
+// eslint-disable-next-line no-undef
+Console.console.log(togglePopup());
