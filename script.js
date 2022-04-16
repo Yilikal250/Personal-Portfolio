5e3be3c7894b5140588458778de087ba48b516a9
Yilikal250 <teshomeyilikal250@gmail.com>
2022-04-16 12:24:18 +0300

    local storage added

--- script.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable linebreak-style */
-// eslint-disable-next-line no-unused-vars
-function togglePopup() {
-  // eslint-disable-next-line indent
-  document.getElementById('popup-1').classList.toggle('active');
-}
-
-// eslint-disable-next-line no-undef
-Console.console.log(togglePopup());
